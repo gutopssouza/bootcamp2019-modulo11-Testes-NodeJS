68e5d856b9ef7ec1da4ee49f5ad11b71ed7269e0
Augusto <gutopssouza@gmail.com>
2019-10-14 19:39:57 -0300

    :construction: Criptografia de senha :rocket: Implement test to check if user password is encrypted when user is registered

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,12 @@ require('../bootstrap');
 module.exports = {
   dialect: process.env.DB_DIALECT || 'postgres',
   host: process.env.DB_HOST,
+  port: 5433,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: 5433,
   storage: './__tests__/database.sqlite',
+  logging: false,
   define: {
     timestamps: true,
     underscored: true,
